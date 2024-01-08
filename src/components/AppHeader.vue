bfe13fcf2240cd3270ecd564f677807d82292ed3
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-08 12:22:13 +0100

    header: inseriti li in ul

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,17 @@
 <template>
     <nav class="my_container container mx-auto">
         <div class="row">
-            <div class="col-3">immagine</div>
-            <div class="col-6">lista</div>
+            <div class="col-3">
+                <img src="" alt="">
+            </div>
+            <ul class="col-6 d-flex justify-content-center">
+                <li class="me-4"><a href="">Home</a></li>
+                <li class="me-4"><a href="">About Us</a></li>
+                <li class="me-4"><a href="">Trainings</a></li>
+                <li class="me-4"><a href="">Packages</a></li>
+                <li class="me-4"><a href="">Blog</a></li>
+                <li class="me-4"><a href="">Contact</a></li>
+            </ul>
             <div class="col-3">- e bottone</div>
         </div>
     </nav>
@@ -22,4 +31,10 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+ul {
+    li {
+        font-size: 0.8rem;
+    }
+}
+</style>
