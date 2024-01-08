594a209150e1fb7442e57890819dd44d740ae581
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-08 12:08:09 +0100

    diviso in col il nav dell' Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        HEADER
-    </div>
+    <nav class="my_container container mx-auto">
+        <div class="row">
+            <div class="col-3">immagine</div>
+            <div class="col-6">lista</div>
+            <div class="col-3">- e bottone</div>
+        </div>
+    </nav>
 </template>
 
 <script>
