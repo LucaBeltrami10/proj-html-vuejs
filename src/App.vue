dad6fcb29c9efe41715f3dc5aa6531543cc38dd1
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-08 11:30:39 +0100

    importati AppHeader, AppMain e AppFooter in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
-<template></template>
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
 export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
 
 }
 </script>
