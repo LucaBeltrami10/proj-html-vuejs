13a83a8adb709242fd3cc2947936f037e76e471a
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-08 12:53:51 +0100

    inserito logo e bottone in header nav

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
 </template>
 
 <script>
-import AppHeader from './components/AppHeader.vue'
-import AppMain from './components/AppMain.vue'
-import AppFooter from './components/AppFooter.vue'
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
   components: {
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,18 +1,25 @@
 <template>
-    <nav class="my_container container mx-auto">
+    <nav class="my_container container mx-auto py-1">
         <div class="row">
             <div class="col-3">
-                <img src="" alt="">
+                <img id="gobike-logo" src="../assets/img/logo-gobike.png" alt="go bike logo">
             </div>
-            <ul class="col-6 d-flex justify-content-center">
-                <li class="me-4"><a href="">Home</a></li>
-                <li class="me-4"><a href="">About Us</a></li>
-                <li class="me-4"><a href="">Trainings</a></li>
-                <li class="me-4"><a href="">Packages</a></li>
-                <li class="me-4"><a href="">Blog</a></li>
-                <li class="me-4"><a href="">Contact</a></li>
+            <ul class="col-6 d-flex justify-content-center align-items-center mb-0">
+                <li class="me-4 fw-semibold"><a href="">Home</a></li>
+                <li class="me-4 fw-semibold"><a href="">About Us</a></li>
+                <li class="me-4 fw-semibold"><a href="">Trainings</a></li>
+                <li class="me-4 fw-semibold"><a href="">Packages</a></li>
+                <li class="me-4 fw-semibold"><a href="">Blog</a></li>
+                <li class="me-4 fw-semibold"><a href="">Contact</a></li>
             </ul>
-            <div class="col-3">- e bottone</div>
+            <div class="col-3 d-flex align-items-center">
+                <i class="fa-solid fa-bars me-2 fs-4"></i>
+                <button type="button" class="my_button btn btn-dark d-flex align-items-center ms-2">
+                    <i class="fa-solid fa-helmet-un fs-3 me-2"></i>
+                    <p class="m-0 fw-semibold">Upcoming Events</p>
+                    <i class="fa-solid fa-arrow-right ms-1"></i>
+                </button>
+            </div>
         </div>
     </nav>
 </template>
@@ -36,5 +43,22 @@ ul {
     li {
         font-size: 0.8rem;
     }
+}
+
+button.my_button {
+    padding: 0.5rem 0.7rem 0.5rem 0.7rem;
+    border-radius: 3px;
+
+    p {
+        font-size: 0.8rem;
+    }
+
+    i.fa-arrow-right {
+        font-size: 0.7rem;
+    }
+}
+
+#gobike-logo {
+    width: 140px;
 }
 </style>
