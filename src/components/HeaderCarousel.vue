2d7a5741c890f8b9466377e2d5764a39b993b9d0
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-08 13:14:09 +0100

    creato nuovo componente: HeaderCarousel

---
+++ src/components/HeaderCarousel.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-itemactive">
+                <img src="../assets/img/event3.jpg" class="d-block w-100" alt="...">
+                <div class="event-info">
+                    <h1>professional Cycling Club</h1>
+                    <p>Learn cycling from the pros.</p>
+                    <button class="btn btn-dark">Learn More <i class="fa-solid fa-arrow-right ms-1"></i></button>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/img/event2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/img/event1.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="">
+    
+</style>
