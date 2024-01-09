11ce009cd85f45950e5fceb8520ed48a495e0a79
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-09 14:40:54 +0100

    creato oggetto in AppMain con info eventi

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,18 @@
 <template>
     <SectionKnowUs />
     <SectionCyclingTrainings :CyclingTrainings="CyclingTrainings" />
+    <SectionKnowUs />
 </template>
 
 <script>
 import SectionKnowUs from './SectionKnowUs.vue';
 import SectionCyclingTrainings from './SectionCyclingTrainings.vue';
+import SectionOurEvents from './SectionOurEvents.vue';
 export default {
     components: {
         SectionKnowUs,
         SectionCyclingTrainings,
+        SectionOurEvents,
 
     },
     data() {
@@ -35,6 +38,109 @@ export default {
                     name: 'Riding Lesson',
                     img: 'training-box-5.jpg'
                 }
+            ],
+            EventsInfo: [
+                {
+                    title: 'Toronto Cycling Event 2023',
+                    startDate: '2022-06-22',
+                    startHours: '01:30',
+                    endDate: '2023-08-11',
+                    endHoures: '07:00',
+                    tag: 'Bicycle Race',
+                    img: 'event1.jpg',
+                    place: 'Toronto',
+                },
+                {
+                    title: 'Toronto Cycling Event 2023',
+                    startDate: '2022-06-22',
+                    startHours: '01:30',
+                    endDate: '2023-08-11',
+                    endHoures: '07:00',
+                    tag: 'Bicycle Race',
+                    img: 'event2.jpg',
+                    place: 'Toronto',
+                },
+                {
+                    title: 'Toronto Cycling Event 2023',
+                    startDate: '2022-06-22',
+                    startHours: '01:30',
+                    endDate: '2023-08-11',
+                    endHoures: '07:00',
+                    tag: 'Bicycle Race',
+                    img: 'event3.jpg',
+                    place: 'Toronto',
+                },
+                {
+                    title: 'Toronto Cycling Event 2023',
+                    startDate: '2022-06-22',
+                    startHours: '01:30',
+                    endDate: '2023-08-11',
+                    endHoures: '07:00',
+                    tag: 'Bicycle Race',
+                    img: 'event4.jpg',
+                    place: 'Toronto',
+                },
+                {
+                    title: 'Toronto Cycling Event 2023',
+                    startDate: '2022-06-22',
+                    startHours: '01:30',
+                    endDate: '2023-08-11',
+                    endHoures: '07:00',
+                    tag: 'Bicycle Race',
+                    img: 'event5.jpg',
+                    place: 'Toronto',
+                },
+                {
+                    title: 'Toronto Cycling Event 2023',
+                    startDate: '2022-06-22',
+                    startHours: '01:30',
+                    endDate: '2023-08-11',
+                    endHoures: '07:00',
+                    tag: 'Bicycle Race',
+                    img: 'event6.jpg',
+                    place: 'Toronto',
+                },
+                {
+                    title: 'Toronto Cycling Event 2023',
+                    startDate: '2022-06-22',
+                    startHours: '01:30',
+                    endDate: '2023-08-11',
+                    endHoures: '07:00',
+                    tag: 'Bicycle Race',
+                    img: 'event7.jpg',
+                    place: 'Toronto',
+                },
+                {
+                    title: 'Toronto Cycling Event 2023',
+                    startDate: '2022-06-22',
+                    startHours: '01:30',
+                    endDate: '2023-08-11',
+                    endHoures: '07:00',
+                    tag: 'Bicycle Race',
+                    img: 'event8.jpg',
+                    place: 'Toronto',
+                },
+                {
+                    title: 'Toronto Cycling Event 2023',
+                    startDate: '2022-06-22',
+                    startHours: '01:30',
+                    endDate: '2023-08-11',
+                    endHoures: '07:00',
+                    tag: 'Bicycle Race',
+                    img: 'event9.jpg',
+                    place: 'Toronto',
+                },
+                {
+                    title: 'Toronto Cycling Event 2023',
+                    startDate: '2022-06-22',
+                    startHours: '01:30',
+                    endDate: '2023-08-11',
+                    endHoures: '07:00',
+                    tag: 'Bicycle Race',
+                    img: 'event10.jpg',
+                    place: 'Toronto',
+                }
+
             ]
         }
     },
