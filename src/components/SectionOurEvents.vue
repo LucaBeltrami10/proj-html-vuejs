6e879ed61cec4df9fffe7abb2bd19c2988e6e7bb
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-09 15:09:59 +0100

    applicato v-for in card SectionOurEvent e inserito contenuto

--- src/components/SectionOurEvents.vue
+++ src/components/SectionOurEvents.vue
@@ -10,24 +10,22 @@
                     <button>burghermenu</button>
                 </div>
             </div>
-            <div class="card-container d-flex">
-                <div class="card">
-                    <img src="/src/assets/img/event10.jpg" class="card-img-top" alt="...">
-                    <div class="card-body text-start">
+            <div class="card-container d-flex flex-wrap">
+                <div v-for="card in EventsInfo" class="card">
+                    <img :src="`/src/assets/img/${card.img}`" class="card-img-top" alt="...">
+                    <div class="card-body text-start pb-0">
                         <h1 class="fw-bold fs-6">Toronto Cycling Event 2023</h1>
                         <div class="event-data-info d-flex align-items-center">
                             <i class="fa-regular fa-clock me-1"></i>
-                            <p class="m-0">2022-06-22</p>
-                            <p class="m-0">01:30</p>
-                            <p class="m-0">2023-08-11</p>
-                            <p class="m-0">07:00</p>
+                            <p class="m-0">{{ card.startDate }} @ {{ card.startHours }} - {{ card.endDate }} @ {{
+                                card.endHours }}</p>
                         </div>
                         <div class="d-flex">
                             <i class="fa-solid fa-location-dot me-1"></i>
-                            <p>Toronto</p>
+                            <p>{{ card.place }}</p>
                         </div>
                         <div class="my_tag">
-                            <p>Bicicle Race</p>
+                            <p class="mb-1">{{ card.tag }}</p>
                         </div>
                     </div>
                 </div>
@@ -50,7 +48,7 @@ div.card-container {
     margin: 1rem 1rem 1rem 1rem;
 
     p {
-        font-size: 0.8rem;
+        font-size: 0.7rem;
     }
 
 }
