712b3819864415cfd5770cb3a9fa6d4022ba0b9a
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-09 15:01:55 +0100

    Completa card di riferimento su cui applicare v-for

--- src/components/SectionOurEvents.vue
+++ src/components/SectionOurEvents.vue
@@ -12,10 +12,23 @@
             </div>
             <div class="card-container d-flex">
                 <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                            card's content.</p>
+                    <img src="/src/assets/img/event10.jpg" class="card-img-top" alt="...">
+                    <div class="card-body text-start">
+                        <h1 class="fw-bold fs-6">Toronto Cycling Event 2023</h1>
+                        <div class="event-data-info d-flex align-items-center">
+                            <i class="fa-regular fa-clock me-1"></i>
+                            <p class="m-0">2022-06-22</p>
+                            <p class="m-0">01:30</p>
+                            <p class="m-0">2023-08-11</p>
+                            <p class="m-0">07:00</p>
+                        </div>
+                        <div class="d-flex">
+                            <i class="fa-solid fa-location-dot me-1"></i>
+                            <p>Toronto</p>
+                        </div>
+                        <div class="my_tag">
+                            <p>Bicicle Race</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -32,5 +45,13 @@ export default {
 }
 </script>
 <style lang="scss">
-div.card-container {}
+div.card-container {
+    width: calc((100% / 3) - 2rem);
+    margin: 1rem 1rem 1rem 1rem;
+
+    p {
+        font-size: 0.8rem;
+    }
+
+}
 </style>
