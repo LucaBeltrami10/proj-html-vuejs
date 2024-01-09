1238460b621f20045273a802f4e29cf19d189f60
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-09 15:49:09 +0100

    creata card generica in OurTrainers

---
+++ src/components/SectionOurTrainers.vue
@@ -0,0 +1,58 @@
+<template>
+    <section class="my_container mx-auto">
+        <div class="w-100 text-center">
+            <h1 class="fw-bold fs-5">Our Expert Trainers</h1>
+            <p>Learn to ride a bike from the expert</p>
+        </div>
+        <div class="w-100 d-flex">
+            <div class="my_card card">
+                <div class="img_container">
+                    <img src="../assets/img/biker1.jpg" class="card-img-top" alt="...">
+                </div>
+
+                <div class="card-body text-center">
+                    <div>
+                        <p class="fw-bold mb-1">name Surname</p>
+
+                        <p class="fw-bold">Biker</p>
+                        <div class="d-flex">
+                            <i class="fa-brands fa-facebook-f" style="color: #ffffff;"></i>
+                            <i class="fa-brands fa-square-instagram" style="color: #ffffff;"></i>
+                            <i class="fa-brands fa-twitter"></i>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </section>
+</template>
+<script>
+export default {
+    props: {
+        OurTrainers: Object
+    }
+
+}
+</script>
+<style lang="scss">
+div.my_card {
+    width: calc((100% / 4) - 2.4rem);
+    margin: 0 1.2rem;
+
+    p {
+        font-size: 0.8rem;
+    }
+
+    div.img_container {
+        height: 160px;
+        overflow: hidden;
+    }
+
+    div.card-body {
+        color: white;
+        background-color: black;
+    }
+}
+</style>
