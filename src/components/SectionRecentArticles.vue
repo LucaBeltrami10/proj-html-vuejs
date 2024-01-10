6ae8e1a7cf1c1802e2acff02cca54f32e9c6e660
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-10 10:49:03 +0100

    creato card generica RecentArticles

--- src/components/SectionRecentArticles.vue
+++ src/components/SectionRecentArticles.vue
@@ -1,5 +1,18 @@
 <template>
     <section class="my_container mx-auto mb-5">
+        <div class="text-center">
+            <h1>Recent New & Articles</h1>
+            <p>important informatios about bike</p>
+        </div>
+        <div class="card_container d-flex">
+            <div class="my_card text-center">
+                <img src="../assets/img/news-bike3-300x180.jpg" alt="">
+                <p class="fw-bold data_news">22.06.2022 - bike</p>
+                <h1 class="fw-bold fs-6">Road bike or mountain bike?</h1>
+                <p>Compared to simila road bikes with a solid frame structure, [...]</p>
+                <button type="button" class="btn btn-dark">More</button>
+            </div>
+        </div>
 
     </section>
 </template>
@@ -8,4 +21,35 @@ export default {
 
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+section.my_container {
+    div.card_container {
+        div.my_card {
+            width: calc((100% / 4) - 2rem);
+            margin: 0 2rem;
+            padding: 0.8rem;
+            border-radius: 5px;
+            box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+
+            p {
+                font-size: 0.6rem;
+
+                &.data_news {
+                    font-size: 0.7rem;
+                }
+            }
+
+            img {
+                width: 100%;
+            }
+
+            button {
+                padding: 0.8rem 1.5rem;
+                border-radius: 3px;
+                font-size: 0.7rem;
+                font-weight: bold;
+            }
+        }
+    }
+}
+</style>
