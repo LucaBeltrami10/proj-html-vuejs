4c624b67359e9dd0560c795fde9376ac7f17767a
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-10 13:45:17 +0100

    carosello header reso dinamico

--- src/components/HeaderCarousel.vue
+++ src/components/HeaderCarousel.vue
@@ -1,7 +1,8 @@
 <template>
-    <div id="carouselExample" class="carousel slide">
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
-            <div v-for="slide, index in SliderContent" class="carousel-item" :class="index === 0 ? 'active' : ''">
+            <div v-for="slide, index in SliderContent" class="carousel-item" :class="index === 0 ? 'active' : ''"
+                data-bs-interval="5000">
                 <img :src="`/src/assets/img/${slide.img}`" class="d-block" alt="Professional Cycling Photos">
                 <div class="event-info">
                     <h1>{{ slide.title }}</h1>
@@ -10,11 +11,13 @@
                 </div>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
@@ -31,7 +34,7 @@ export default {
 }
 </script>
 <style lang="scss">
-#carouselExample {
+#carouselExampleAutoplaying {
     position: relative;
     width: 100%;
     height: 550px;
